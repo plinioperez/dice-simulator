f80fff03ec8dd7978762c95976860eecc19df400
plinioperez <hppp10@gmail.com>
2025-06-01 11:10:13 -0400

    agrega código base

---
+++ dice_simulator.py
@@ -0,0 +1,14 @@
+import dice
+import time
+
+def lanzar_dados(amount, sides):
+    resultados = []
+    for i in range(amount):
+        resultado = dice.roll(f'd{sides}')
+        resultados.append(resultado)
+        print(f"Lanzamiento {i + 1} número obtenido {resultado}")
+        time.sleep(5)
+    return resultados
+
+if __name__ == "__main__":
+    lanzar_dados(amount=5, sides=6)
